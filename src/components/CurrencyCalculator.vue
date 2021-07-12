e78616c151f6dc574c90c2b5623e384ce1965f9c
obsequey <34972991+obsequey@users.noreply.github.com>
2021-07-12 15:40:47 +0300

    Configure validation and sending from form

--- src/components/CurrencyCalculator.vue
+++ src/components/CurrencyCalculator.vue
@@ -1,15 +1,18 @@
 <template>
   <v-container v-if="isLoading">Loading</v-container>
-  <v-form v-else>
+  <v-form @submit.prevent="sendEmail" v-else>
     <v-container mt-6>
       <v-row>
         <!-- Value to transform -->
         <v-col cols="12" sm="6" md="4">
           <v-text-field
+            ref="inputValue"
             filled
+            required
             label="Сумма в рублях"
             v-model="inputValue"
             type="number"
+            :rules="[validationRules.required]"
             prepend-icon="mdi-currency-rub"
             hide-details="auto"
           ></v-text-field>
@@ -18,11 +21,14 @@
         <v-col cols="12" sm="6" md="4">
           <!-- Transform to which currency -->
           <v-autocomplete
+            ref="selectedCurrency"
             filled
+            required
             v-model="selectedCurrency"
             :items="currencyList"
             label="Целевая валюта"
             prepend-icon="mdi-arrow-right"
+            :rules="[validationRules.required]"
           >
           </v-autocomplete>
         </v-col>
@@ -46,24 +52,25 @@
       <v-row class="justify-center">
         <v-col cols="12" sm="6">
           <v-text-field
+            ref="sendToEmail"
             filled
+            required
             label="Куда отправить результаты?"
             v-model="sendToEmail"
-            :rules="emailValidationRules"
+            :rules="[validationRules.required, validationRules.email]"
             prepend-icon="mdi-email"
           ></v-text-field>
         </v-col>
       </v-row>
 
       <v-btn
-        @click.prevent=""
         :loading="sendingEmail"
         :disabled="sendingEmail"
+        type="submit"
         class="d-block mt-2 mx-auto"
-        color="primary"
-        dark
-        >Отправить</v-btn
-      >
+        color="secondary"
+        >Отправить
+      </v-btn>
     </v-container>
   </v-form>
 </template>
@@ -81,13 +88,13 @@ export default {
     isLoading: false,
     sendToEmail: '',
     sendingEmail: false,
-    emailValidationRules: [
-      (value) => !!value || 'Required.',
-      (value) => {
+    validationRules: {
+      required: (value) => !!value || 'Required field.',
+      email: (value) => {
         const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         return pattern.test(value) || 'Неверно введена почта'
       }
-    ]
+    }
   }),
 
   mounted() {
@@ -99,7 +106,7 @@ export default {
       /** Info about valute rates are stored in a Object { VALUTENAME: {}, ...}
        * So we parse it by values and return the resulting array
        * @return Array */
-      function getCurrencyValuesFromJSON(json) {
+      const getCurrencyValuesFromJSON = (json) => {
         return Object.values(json.Valute).map((item) => item.Name)
       }
 
@@ -119,12 +126,39 @@ export default {
         return
       }
 
+      // Get the value from localStorage if it already exists
       this.currencyList = getCurrencyValuesFromJSON(
         JSON.parse(localStorage.getItem('fetchedCurrencyList'))
       )
     },
 
-    sendEmail() {}
+    async sendEmail() {
+      const validateWholeForm = () => {
+        const form = ['inputValue', 'selectedCurrency', 'sendToEmail']
+        form.forEach((field) => this.$refs[field].validate(true))
+      }
+
+      if (!this.formIsReady) {
+        validateWholeForm()
+        return
+      }
+
+      this.sendingEmail = true
+      await fetch('http://localhost:3000/email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: '*/*'
+        },
+        body: JSON.stringify({
+          message: this.messageToSendToEmail,
+          sendToEmail: this.sendToEmail
+        })
+      }).then((res) => {
+        console.log(res)
+      })
+      this.sendingEmail = false
+    }
   },
   computed: {
     // Find the selected currency object in localStorage object
@@ -144,6 +178,23 @@ export default {
       return this.selectedCurrencyObject
         ? this.inputValue / this.selectedCurrencyObject.Value
         : 0
+    },
+
+    messageToSendToEmail() {
+      return `
+Hey!<br>
+<br>
+Here are your results for currency conversion:<br>
+${this.inputValue} RUB to ${this.selectedCurrency} at rate ${this.selectedCurrencyObject.Value}<br>
+------<br>
+${this.valueInTargetCurrency}<br>
+<br>
+Have a nice day :)
+`
+    },
+
+    formIsReady() {
+      return !!this.inputValue && !!this.selectedCurrency && !!this.sendToEmail
     }
   }
 }
